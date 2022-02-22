ec43c61ef5606266d44a602aeb2c7040a99c0cc6
Ricael Pereira Barbosa <85632605+RicaelBarbosa@users.noreply.github.com>
2022-02-22 11:36:50 -0300

    Add files via upload

---
+++ Js alura/Formação JavaScript/Fundamentos do JavaScript/Trabalhando com strings.js
@@ -0,0 +1,12 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte"
+console.log(cidade === input); // fals
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte"
+
+const inputMinusculo = input.toLowerCase()
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
---
+++ Js alura/Formação JavaScript/Fundamentos do JavaScript/type-Number.js
@@ -0,0 +1,25 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operaçãoMatemática = primeiroNumero + segundoNumero
+
+console.log(operaçãoMatemática)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3
+const pontoFlutuanteSemZero = .5
+
+const novaOperação = primeiroNumero / numeroPontoFlutuante
+
+console.log (novaOperação)
+
+// NaN not a Number (Não é um número)
+
+const Alura = "Alura"
+
+console.log(Alura * primeiroNumero)
